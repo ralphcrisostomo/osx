8caeec5488025be22eb9d6246423d7d3655e747e
Ralph Crisostomo <rrecrisostomo@gmail.com>
2014-11-30 17:02:19 +0800

    Updated :)

---
+++ scripts/clone.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# DESCRIPTION
+# Clone github repository.
+
+# PROJECTS
+
+# LittlKnot Node v2
+install_git_app "git@github.com:magnuky/littleknot-node-v2.git" "$HOME/Projects/_projects"
+
+# GRUNT INIT
+
+# Node Coffee
+install_git_app "git@github.com:ralphcrisostomo/grunt-init-node-coffee.git" "$HOME/Projects/_projects/node-coffee"
+
+# Grunt Plugin Coffee
+install_git_app "git@github.com:ralphcrisostomo/grunt-init-grunt-plugin-coffee.git" "$HOME/Projects/_projects/grunt-plugin-coffee"
+
+
